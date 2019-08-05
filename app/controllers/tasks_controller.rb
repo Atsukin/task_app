2b232497121b07e83f755c3c7d9fafef9303d223
Atsukin <atsuki.tamura.2740@gmail.com>
2019-08-05 12:26:49 +0900

    ステータス機能を追加しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,6 +42,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name,:description,:limit)
+    params.require(:task).permit(:name,:description,:limit,:status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  enum status: { notstart: 0, launch: 1, complete: 2 }
+    class << self
+    def localed_statuses
+      statuses.keys.map do |s|
+        [ApplicationController.helpers.t("status.task.#{s}"), s]
+      end
+    end
+  end
 end
