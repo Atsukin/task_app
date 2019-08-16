c742ccbc13909e11b4857717781cae478d2923e3
Atsukin <atsuki.tamura.2740@gmail.com>
2019-08-16 21:44:22 +0900

    validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
+  before_destroy :confirm_none_admin_user
   has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
   has_many :tasks
+
+  private
+  def confirm_none_admin_user
+    if User.where(admin: true).count==1
+      errors.add(:admin, 'は０にすることはできません。')
+    else
+      errors.add(:admin, 'は1にすることはできません。')
+    end
+  end
 end
