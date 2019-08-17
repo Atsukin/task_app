2ab16273188d15ce131e549e8ec40eeda4570d36
Atsukin <atsuki.tamura.2740@gmail.com>
2019-08-17 09:42:27 +0900

    labelを作りました。

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, length: { maximum: 30 }
 
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 
   enum status: { notstart: 0, launch: 1, complete: 2 }
   enum priority: { high: 0, middle: 1, low: 2 }
